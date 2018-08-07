3caf44057d911ec2c53e7778e313f4b318ce2598
sevenflash <sevenflash@yandex.ru>
2018-08-08 01:05:06 +0300

    change ropsten to rinkeby

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   ethbtc: {
-    rpc: 'https://api.myetherapi.com/rop',
+    rpc: 'https://rinkeby.infura.io/',
     contract: ' 0x4356152f044E3a1Ce1A57566b2E0BEe57949c1b2',
     swapName: 'ethbtc',
     reputationName: 'rating',
