a26e7fb9e44ae4c39715b8f887582bde0ea5a1b6
sevenflash <sevenflash@yandex.ru>
2018-08-07 21:42:31 +0300

    fix event names

--- streams/ethbtc.js
+++ streams/ethbtc.js
@@ -7,19 +7,19 @@ let swapRefundedInstance = null
 module.exports = {
   SwapCreated: ({ web3, contract, fromBlock }) => {
     if (swapCreatedInstance === null)
-      swapCreatedInstance = new Web3EventStream(web3, contract.Created, {}, { fromBlock })
+      swapCreatedInstance = new Web3EventStream(web3, contract.CreateSwap, {}, { fromBlock })
 
     return swapCreatedInstance
   },
   SwapWithdrawn: ({ web3, contract, fromBlock }) => {
     if (swapWithdrawnInstance === null)
-      swapWithdrawnInstance = new Web3EventStream(web3, contract.Withdrawn, {}, { fromBlock })
+      swapWithdrawnInstance = new Web3EventStream(web3, contract.Withdraw, {}, { fromBlock })
 
     return swapWithdrawnInstance
   },
   SwapRefunded: ({ web3, contract, fromBlock }) => {
     if (swapRefundedInstance === null)
-      swapRefundedInstance = new Web3EventStream(web3, contract.Refunded, {}, { fromBlock })
+      swapRefundedInstance = new Web3EventStream(web3, contract.Refund, {}, { fromBlock })
 
     return swapRefundedInstance
   }
