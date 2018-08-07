d2311a9a2e4f378b24b10bfbebc1e84e70252c52
sevenflash <sevenflash@yandex.ru>
2018-08-08 01:09:44 +0300

    fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   ethbtc: {
     rpc: 'https://rinkeby.infura.io/',
-    contract: ' 0x4356152f044E3a1Ce1A57566b2E0BEe57949c1b2',
+    contract: '0x4356152f044E3a1Ce1A57566b2E0BEe57949c1b2',
     swapName: 'ethbtc',
     reputationName: 'rating',
     pointsIncrease: 1,
